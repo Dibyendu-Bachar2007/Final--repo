be762d2069edaf84d1785952c566db5e7ca8ec18
Dibyendu-Bachar2007 <dibyendubachar08@gmail.com>
2026-01-04 22:54:09 +0530

    Update Script.js

--- Script.js
+++ Script.js
@@ -1,6 +1,18 @@
-function showMore() {
-    const sections = document.querySelectorAll('.hidden');
-    sections.forEach(section => {
-        section.style.display = "block";
+function sendMsg() {
+    let email = document.getElementById("email").value;
+    let msg = document.getElementById("msg");
+
+    if (email === "") {
+        msg.innerText = "Please enter your email.";
+        msg.style.color = "red";
+    } else {
+        msg.innerText = "Message sent successfully!";
+        msg.style.color = "green";
+    }
+}
+
+function scrollToSec(id) {
+    document.getElementById(id).scrollIntoView({
+        behavior: "smooth"
     });
 }
